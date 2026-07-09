a95c4202c512d90367d33fe30d1f26475dc782fe
fariseslamicbin-code <fariseslamicbin@gmail.com>
2026-07-10 01:12:46 +0200

    Add files via upload

---
+++ Color.kt
@@ -0,0 +1,17 @@
+package com.lifesimulator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Background = Color(0xFF0A0A0F)
+val Surface = Color(0xFF111118)
+val Surface2 = Color(0xFF16161F)
+val Border = Color(0x12FFFFFF)
+val Border2 = Color(0x1FFFFFFF)
+val TextPrimary = Color(0xFFE8E8F0)
+val TextMuted = Color(0xFF6B6B80)
+val Accent = Color(0xFFF0B429)
+val Accent2 = Color(0xFFE07D10)
+val Green = Color(0xFF34D399)
+val Red = Color(0xFFF87171)
+val Blue = Color(0xFF60A5FA)
+val Purple = Color(0xFFA78BFA)
